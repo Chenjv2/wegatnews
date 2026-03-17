8bedf7aee0d0449213ffd7bd52f06056d1b4fd08
Chenjv2 <chenjv2.vcbs@gmail.com>
2026-03-17 14:34:46 +0100

    welcome text

--- pages/index.js
+++ pages/index.js
@@ -14,46 +14,53 @@ export async function getStaticProps() {
 
 function DisplayContainer({ data }) {
   return (
-    <>
-      <div className="blog-container">
-        {data.map((post) => (
-          <div key={post.slug} className="blog-entry">
-            <img src={post.image} alt="blog-foto" />
-            <h3>{post.title}</h3>
-            <p>{post.preview}...</p>
+    <div className="blog-container">
+      {data.map((post) => (
+        <div key={post.slug} className="blog-entry">
+          <img src={post.image} alt="blog-foto" />
+          <h3>{post.title}</h3>
+          <p>{post.preview}...</p>
 
-            <div className="meta">
-              {post.date} · {post.author}
-            </div>
-
-            <Link href={`/blog/${post.slug}`}>Weiterlesen</Link>
+          <div className="meta">
+            {post.date} · {post.author}
           </div>
-        ))}
-      </div>
-    </>
+
+          <Link href={`/blog/${post.slug}`}>Weiterlesen</Link>
+        </div>
+      ))}
+    </div>
   );
 }
 
 function NewestPosts({ posts }) {
   return (
-    <>
-      <section className="blog-section">
-        <DisplayContainer data={posts.slice(0, 4)} />
-        <DisplayContainer data={posts.slice(4)} />
-      </section>
-    </>
+    <section className="blog-section">
+      <DisplayContainer data={posts.slice(0, 4)} />
+      <DisplayContainer data={posts.slice(4)} />
+    </section>
   );
 }
 
 export default function Blog({ newestPosts, archivePosts }) {
   return (
     <>
+      {/* Welcome text */}
+      <div className="welcome-container">
+        <h2 className="welcome">
+          Willkommen auf wegatnews, der digitalen Schülerzeitung am Altenforst.
+        </h2>
+      </div>
+
+      {/* Newest blog posts */}
       <NewestPosts posts={newestPosts} />
+
+      {/* Contact section */}
       <section>
         <div className="image-or-text">
           <h2>Ideen, Artikel, Probleme, Fragen?</h2>
           <p>
-            Einfach per Teams oder E-Mail an Vincent Cui, vincent.cui@altenforst.de
+            Einfach per Teams oder E-Mail an Vincent Cui,
+            vincent.cui@altenforst.de
           </p>
           <p>
             Wir treffen uns jeden Donnerstag in der Mittagspause vor den
@@ -62,13 +69,14 @@ export default function Blog({ newestPosts, archivePosts }) {
           <p>Komm doch einfach mal vorbei!</p>
         </div>
       </section>
+
+      {/* Archive */}
       <section>
         <h1 className="archiv">Archiv</h1>
         <ul className="archiv">
           {archivePosts.map((post) => (
             <li key={post.slug}>
-              <Link href={`/blog/${post.slug}`}>{post.title}</Link>
-              {post.date}
+              <Link href={`/blog/${post.slug}`}>{post.title}</Link> {post.date}
             </li>
           ))}
         </ul>
