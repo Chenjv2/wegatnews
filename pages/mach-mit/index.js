ced6b8f48a0a29a9465838160e170a73df3bec31
Chenjv2 <chenjv2.vcbs@gmail.com>
2026-04-15 19:02:46 +0200

    Fehler Zähler

--- pages/mach-mit/index.js
+++ pages/mach-mit/index.js
@@ -2,19 +2,50 @@ export default function InformativIndex() {
   return (
     <main>
       <section>
-        <div className="image-or-text">
-          <h2>Ideen, Artikel, Probleme, Fragen?</h2>
-          <p>
-            Einfach per Teams oder E-Mail an Vincent Cui (Technik), oder an Ouijdan Hussein (Inhalt)
-          </p>
-          <p>
-            vincent.cui@altenforst.de, ouijdan.hussein@altenforst.de
-          </p>
-          <p>
-            Wir treffen uns jeden Donnerstag in der Mittagspause vor den
-            Computerräumen.
-          </p>
-          <p>Komm doch einfach mal vorbei!</p>
+        <div className="container">
+          <div className="image-or-text">
+            <h2>Ideen, Artikel, Probleme, Fragen?</h2>
+            <p>
+              Einfach per Teams oder E-Mail an Vincent Cui (Technik), oder an
+              Ouijdan Hussein (Inhalt)
+            </p>
+            <p>vincent.cui@altenforst.de, ouijdan.hussein@altenforst.de</p>
+            <p>
+              Wir treffen uns jeden Donnerstag in der Mittagspause vor den
+              Computerräumen.
+            </p>
+            <p>Komm doch einfach mal vorbei!</p>
+          </div>
+          <div className="image-or-text">
+            <h2>Rechtschreibfehler Zähler</h2>
+            <h3>81</h3>
+            <p>
+              Rechtschreibfehler wurden bisher gefunden. Vielen Dank an alle
+              fleißigen Leser*innen, die uns auf Fehler aufmerksam machen!
+            </p>
+            <p>
+              <b>Platz 1</b> der Rechtschreibfehlerfinder: ChatGPT 5 mit 15
+              gefundenen Fehlern
+            </p>
+          </div>
+          <div className="image-or-text">
+            <h2>Inhaltliche Fehler Zähler</h2>
+            <h3>1</h3>
+            <p>
+              Inhaltliche Fehler wurden bisher gefunden. Vielen Dank an alle
+              fleißigen Leser*innen, die uns auf Fehler aufmerksam machen!
+            </p>
+            <p>
+              <b>Platz 1</b> der Inhaltliche Fehlerfinder: Unbekannt mit 1
+              gefundenen Fehlern
+            </p>
+          </div>
+          <div className="image-or-text">
+            <h2>Finde auch Fehler!</h2>
+            <p>
+              Hilf uns die Schülerzeitung zu verbessern: Einfach Artikellink, Fehlerbeschreibung und Fehlerart an vincent.cui@altenforst.de 
+            </p>
+          </div>
         </div>
       </section>
     </main>
