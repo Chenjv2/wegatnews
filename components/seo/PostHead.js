4c105657a94d6c313fe65e684b039c3c77e6c0c9
Chenjv2 <chenjv2.vcbs@gmail.com>
2026-04-22 18:40:35 +0200

    new posthead for better preview when sharing

--- components/seo/PostHead.js
+++ components/seo/PostHead.js
@@ -2,15 +2,24 @@ import Head from "next/head";
 
 export default function PostHead({ post }) {
   const description = post.preview || post.title;
+  const url = `https://wegatnews.de/blog/${post.slug}`;
+  const image = post.image
+    ? post.image.startsWith("http")
+      ? post.image
+      : `https://wegatnews.de${post.image}`
+    : null;
 
   return (
     <Head>
       <title>{post.title} | WE GAT NEWS</title>
       <meta name="description" content={description} />
+
       <meta property="og:title" content={post.title} />
+      <meta property="og:site_name" content="WE GAT NEWS" />
       <meta property="og:description" content={description} />
       <meta property="og:type" content="article" />
-      {post.image ? <meta property="og:image" content={post.image} /> : null}
+      <meta property="og:url" content={url} />
+      {image ? <meta property="og:image" content={image} /> : null}
     </Head>
   );
 }
